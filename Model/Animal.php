e4590c8d6c86a3b5ca6673658d5296bf861914bf
Samgrass <samuelegrassetti01@gmail.com>
2024-09-05 00:13:41 +0200

    remove breed from Animal class

--- Model/Animal.php
+++ Model/Animal.php
@@ -3,14 +3,12 @@
 class Animal
 {
     public $species;
-    public $breed;
     public $age;
     public $size;
 
-    public function __construct(String $_species, String $_breed, Int $_age, String $_size)
+    public function __construct(String $_species, Int $_age, String $_size)
     {
         $this->species = $_species;
-        $this->breed = $_breed;
         $this->age = $_age;
         $this->size = $_size;
     }
